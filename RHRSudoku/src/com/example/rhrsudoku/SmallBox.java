1f240365d0904878d1e1be7c756829ea4d939fb4
Hassan Ali <hassan.ali.3821@gmail.com>
2013-01-20 21:39:07 +0000

    added SmallBox.java file

---
+++ RHRSudoku/src/com/example/rhrsudoku/SmallBox.java
@@ -0,0 +1,13 @@
+package com.example.rhrsudoku;
+
+import android.content.Context;
+import android.view.View;
+
+public class SmallBox extends View {
+
+	public SmallBox(Context context) {
+		super(context);
+		// TODO Auto-generated constructor stub
+	}
+	
+}
