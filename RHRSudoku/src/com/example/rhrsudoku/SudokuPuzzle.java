0af812cfe2b6db1250962f1664cb0cf22bff35d5
Hassan Ali <hassan.ali.3821@gmail.com>
2013-01-23 14:33:21 +0000

    create SudokuPuzzle

---
+++ RHRSudoku/src/com/example/rhrsudoku/SudokuPuzzle.java
@@ -0,0 +1,14 @@
+package com.example.rhrsudoku;
+
+public class SudokuPuzzle {
+	/*
+	 * This class represent a sudoku puzzle
+	 */
+	
+	SmallBox[][] puzzle;
+	
+	public SudokuPuzzle() {
+		puzzle = new SmallBox[9][9];
+	}
+
+}
