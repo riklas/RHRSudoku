ca3417d56b7292bb6d025efa40f341e35ca2acf2
Hassan Ali <hassan.ali.3821@gmail.com>
2013-01-23 17:32:43 +0000

    completed SmallBox.java

--- RHRSudoku/src/com/example/rhrsudoku/SmallBox.java
+++ RHRSudoku/src/com/example/rhrsudoku/SmallBox.java
@@ -4,10 +4,36 @@ import android.content.Context;
 import android.view.View;
 
 public class SmallBox extends View {
+	
+	SmallBox[] neighbours;
+	int finalValue;
+	int ID;
+	boolean hasFinalValue;
+	int[] possibleValues;
+	boolean isEditable;
+	boolean isSelected;
 
 	public SmallBox(Context context) {
 		super(context);
 		// TODO Auto-generated constructor stub
+	}
+	
+	public SmallBox(Context context, SmallBox[] neighbours) {
+		super(context);
+		this.neighbours = neighbours;
+	}
+	
+	boolean isConflicting() {
+		if (!hasFinalValue)
+			return false;
+		for (SmallBox sb : neighbours)
+			if (sb.hasFinalValue && (sb.finalValue == finalValue))
+				return true;
+		return false;
+	}
+	
+	void setFinalValue(int finalValue) {
+		this.finalValue = finalValue;
 	}
 	
 }
--- RHRSudoku/src/com/example/rhrsudoku/SudokuPuzzle.java
+++ RHRSudoku/src/com/example/rhrsudoku/SudokuPuzzle.java
@@ -5,10 +5,33 @@ public class SudokuPuzzle {
 	 * This class represent a sudoku puzzle
 	 */
 	
-	SmallBox[][] puzzle;
+	private SmallBox[] puzzle;
 	
-	public SudokuPuzzle() {
-		puzzle = new SmallBox[9][9];
+//	public SudokuPuzzle() {
+//		SmallBox[][] puzzle2 = new SmallBox[9][9];
+//		for (int i=0;i<puzzle2.length;i++)
+//			for (int j=0;j<puzzle2[i].length;j++)
+//				puzzle2[i][j] = new SmallBox();
+//		setPuzzle(puzzle2);
+//	}
+	
+	public SudokuPuzzle(SmallBox[] puzzle) {
+		setPuzzle(puzzle);
+	}
+	
+	private void setPuzzle(SmallBox[] puzzle) {
+		this.puzzle = puzzle;
+	}
+	
+	boolean isConflicting() {
+		// Conflicting is defined as no two numbers in the same row/column/big box conflicting
+			for (SmallBox box : puzzle)
+				if (box.isConflicting())
+					return true;
+		return false;
 	}
 
 }
+
+
+
