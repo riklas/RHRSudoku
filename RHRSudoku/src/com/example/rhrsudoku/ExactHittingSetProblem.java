f363c593f79246f5cdd59dfe999c81a824ea76b9
Hassan Ali <hassan.ali.3821@gmail.com>
2013-01-27 15:57:13 +0000

    added some files

---
+++ RHRSudoku/src/com/example/rhrsudoku/ExactHittingSetProblem.java
@@ -0,0 +1,23 @@
+package com.example.rhrsudoku;
+
+import java.util.Set;
+
+public class ExactHittingSetProblem<E> {
+	/*
+	 * represents an exact hitting set problem
+	 */
+	
+	Set<E> setX;
+	Set<Set<E>> setS;
+	
+	/*
+	 * The problem is to find a subset X* of setX such that each set in setS is
+	 * hit by exactly one element in X*
+	 * the problem is equivalent to the exact cover problem, and is NP-complete
+	 * 
+	 * Exact Hitting Set Problem
+	 * =========================
+	 * Each member of S contains the 
+	 */
+	
+}
---
+++ RHRSudoku/src/com/example/rhrsudoku/ExactHittingSetSolver.java
@@ -0,0 +1,13 @@
+package com.example.rhrsudoku;
+
+import java.util.Set;
+
+public class ExactHittingSetSolver<E> {
+	
+	public Set<E> solve(ExactHittingSetProblem<E> exact) {
+		
+		
+		return null;
+	}
+	
+}
