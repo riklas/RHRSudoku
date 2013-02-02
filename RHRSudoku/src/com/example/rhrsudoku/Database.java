ebb1c2e6121513b79194217be4a1f9950410c0db
riklas <rikins21@gmail.com>
2013-02-02 21:35:49 +0000

    new syntax for hardcoded puzzle, imrpoved readability

--- RHRSudoku/src/com/example/rhrsudoku/Database.java
+++ RHRSudoku/src/com/example/rhrsudoku/Database.java
@@ -2,7 +2,7 @@ package com.example.rhrsudoku;
 
 public class Database {
 
-	int[][] hardcodePuzzle = new int[8][8];
+	int[][] hardcodePuzzle;// = new int[8][8];
 	
 	public Database() {
 		// TODO Auto-generated constructor stub
@@ -10,87 +10,19 @@ public class Database {
 	}
 
 	private void sethardcodePuzzle() {
-		hardcodePuzzle[0][0] = 5;
-		hardcodePuzzle[0][1] = 3;
-		hardcodePuzzle[0][2] = 0;
-		hardcodePuzzle[0][3] = 0;
-		hardcodePuzzle[0][4] = 7;
-		hardcodePuzzle[0][5] = 0;
-		hardcodePuzzle[0][6] = 0;
-		hardcodePuzzle[0][7] = 0;
-		hardcodePuzzle[0][8] = 0;
-		hardcodePuzzle[1][0] = 6;
-		hardcodePuzzle[1][1] = 0;
-		hardcodePuzzle[1][2] = 0;
-		hardcodePuzzle[1][3] = 1;
-		hardcodePuzzle[1][4] = 9;
-		hardcodePuzzle[1][5] = 5;
-		hardcodePuzzle[1][6] = 0;
-		hardcodePuzzle[1][7] = 0;
-		hardcodePuzzle[1][8] = 0;
-		hardcodePuzzle[2][0] = 0;
-		hardcodePuzzle[2][1] = 9;
-		hardcodePuzzle[2][2] = 8;
-		hardcodePuzzle[2][3] = 0;
-		hardcodePuzzle[2][4] = 0;
-		hardcodePuzzle[2][5] = 0;
-		hardcodePuzzle[2][6] = 0;
-		hardcodePuzzle[2][7] = 6;
-		hardcodePuzzle[2][8] = 0;
-		hardcodePuzzle[3][0] = 8;
-		hardcodePuzzle[3][1] = 0;
-		hardcodePuzzle[3][2] = 0;
-		hardcodePuzzle[3][3] = 0;
-		hardcodePuzzle[3][4] = 6;
-		hardcodePuzzle[3][5] = 0;
-		hardcodePuzzle[3][6] = 0;
-		hardcodePuzzle[3][7] = 0;
-		hardcodePuzzle[3][8] = 3;
-		hardcodePuzzle[4][0] = 4;
-		hardcodePuzzle[4][1] = 0;
-		hardcodePuzzle[4][2] = 0;
-		hardcodePuzzle[4][3] = 8;
-		hardcodePuzzle[4][4] = 0;
-		hardcodePuzzle[4][5] = 3;
-		hardcodePuzzle[4][6] = 0;
-		hardcodePuzzle[4][7] = 0;
-		hardcodePuzzle[4][8] = 1;
-		hardcodePuzzle[5][0] = 7;
-		hardcodePuzzle[5][1] = 0;
-		hardcodePuzzle[5][2] = 0;
-		hardcodePuzzle[5][3] = 0;
-		hardcodePuzzle[5][4] = 2;
-		hardcodePuzzle[5][5] = 0;
-		hardcodePuzzle[5][6] = 0;
-		hardcodePuzzle[5][7] = 0;
-		hardcodePuzzle[5][8] = 6;
-		hardcodePuzzle[6][0] = 0;
-		hardcodePuzzle[6][1] = 6;
-		hardcodePuzzle[6][2] = 0;
-		hardcodePuzzle[6][3] = 0;
-		hardcodePuzzle[6][4] = 0;
-		hardcodePuzzle[6][5] = 0;
-		hardcodePuzzle[6][6] = 2;
-		hardcodePuzzle[6][7] = 8;
-		hardcodePuzzle[6][8] = 0;
-		hardcodePuzzle[7][0] = 0;
-		hardcodePuzzle[7][1] = 0;
-		hardcodePuzzle[7][2] = 0;
-		hardcodePuzzle[7][3] = 4;
-		hardcodePuzzle[7][4] = 1;
-		hardcodePuzzle[7][5] = 9;
-		hardcodePuzzle[7][6] = 0;
-		hardcodePuzzle[7][7] = 0;
-		hardcodePuzzle[7][8] = 5;
-		hardcodePuzzle[8][0] = 0;
-		hardcodePuzzle[8][1] = 0;
-		hardcodePuzzle[8][2] = 0;
-		hardcodePuzzle[8][3] = 0;
-		hardcodePuzzle[8][4] = 8;
-		hardcodePuzzle[8][5] = 0;
-		hardcodePuzzle[8][6] = 0;
-		hardcodePuzzle[8][7] = 7;
-		hardcodePuzzle[8][8] = 9;
+		
+		hardcodePuzzle = new int[][] {
+				{5,3,0,0,7,0,0,0,0},
+				{6,0,0,1,9,5,0,0,0},
+				{0,9,8,0,0,0,0,6,0},
+				{8,0,0,0,6,0,0,0,3},
+				{4,0,0,8,0,3,0,0,1},
+				{7,0,0,0,2,0,0,0,6},
+				{0,6,0,0,0,0,2,8,0},
+				{0,0,0,4,1,9,0,0,5},
+				{0,0,0,0,8,0,0,7,9}
+		};
+		
 	}
 	
 }
