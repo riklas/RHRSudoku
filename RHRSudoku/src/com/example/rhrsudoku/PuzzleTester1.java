c5495907fcee710d3c2dcc571553063159add193
Hassan Ali <hassan.ali.3821@gmail.com>
2013-02-04 04:42:26 +0000

    nothing

--- RHRSudoku/src/com/example/rhrsudoku/PuzzleTester1.java
+++ RHRSudoku/src/com/example/rhrsudoku/PuzzleTester1.java
@@ -12,7 +12,7 @@ import exactCover.Quant;
 public class PuzzleTester1 {
 	public static void main(String[] args) {
 		PuzzleTester1 puzzleTester = new PuzzleTester1();
-		puzzleTester.runPuzzle3();
+		puzzleTester.runPuzzle4();
 	}
 	
 	void runPuzzle1() {
@@ -28,72 +28,72 @@ public class PuzzleTester1 {
 	
 	void runPuzzle2() {
 		SudokuPuzzle puzzle = new SudokuPuzzle();
-		puzzle.puzzle[0][0].setValue(1, SudokuPuzzleCell.GENERATED);
-		puzzle.puzzle[0][1].setValue(2, SudokuPuzzleCell.GENERATED);
-		puzzle.puzzle[0][2].setValue(3, SudokuPuzzleCell.GENERATED);
-		puzzle.puzzle[0][3].setValue(4, SudokuPuzzleCell.GENERATED);
-		puzzle.puzzle[0][4].setValue(5, SudokuPuzzleCell.GENERATED);
-		puzzle.puzzle[0][5].setValue(6, SudokuPuzzleCell.GENERATED);
-		puzzle.puzzle[0][6].setValue(7, SudokuPuzzleCell.GENERATED);
-		puzzle.puzzle[0][7].setValue(8, SudokuPuzzleCell.GENERATED);
-		puzzle.puzzle[0][8].setValue(9, SudokuPuzzleCell.GENERATED);
-		
-		puzzle.puzzle[1][0].setValue(7, SudokuPuzzleCell.GENERATED);
-		puzzle.puzzle[1][1].setValue(8, SudokuPuzzleCell.GENERATED);
-		puzzle.puzzle[1][2].setValue(9, SudokuPuzzleCell.GENERATED);
-		puzzle.puzzle[1][3].setValue(1, SudokuPuzzleCell.GENERATED);
-		puzzle.puzzle[1][4].setValue(2, SudokuPuzzleCell.GENERATED);
-		puzzle.puzzle[1][5].setValue(3, SudokuPuzzleCell.GENERATED);
-		puzzle.puzzle[1][6].setValue(4, SudokuPuzzleCell.GENERATED);
-		puzzle.puzzle[1][7].setValue(5, SudokuPuzzleCell.GENERATED);
-		puzzle.puzzle[1][8].setValue(6, SudokuPuzzleCell.GENERATED);
-		
-		puzzle.puzzle[2][0].setValue(4, SudokuPuzzleCell.GENERATED);
-		puzzle.puzzle[2][1].setValue(5, SudokuPuzzleCell.GENERATED);
-		puzzle.puzzle[2][2].setValue(6, SudokuPuzzleCell.GENERATED);
-		puzzle.puzzle[2][3].setValue(7, SudokuPuzzleCell.GENERATED);
-		puzzle.puzzle[2][4].setValue(8, SudokuPuzzleCell.GENERATED);
-		puzzle.puzzle[2][5].setValue(9, SudokuPuzzleCell.GENERATED);
-		puzzle.puzzle[2][6].setValue(1, SudokuPuzzleCell.GENERATED);
-		puzzle.puzzle[2][7].setValue(2, SudokuPuzzleCell.GENERATED);
-		puzzle.puzzle[2][8].setValue(3, SudokuPuzzleCell.GENERATED);
-		
-		puzzle.puzzle[3][0].setValue(5, SudokuPuzzleCell.GENERATED);
-		puzzle.puzzle[3][1].setValue(1, SudokuPuzzleCell.GENERATED);
-		puzzle.puzzle[3][2].setValue(2, SudokuPuzzleCell.GENERATED);
-		puzzle.puzzle[3][3].setValue(6, SudokuPuzzleCell.GENERATED);
-		puzzle.puzzle[3][4].setValue(4, SudokuPuzzleCell.GENERATED);
-		puzzle.puzzle[3][5].setValue(7, SudokuPuzzleCell.GENERATED);
-		puzzle.puzzle[3][6].setValue(9, SudokuPuzzleCell.GENERATED);
-		puzzle.puzzle[3][7].setValue(3, SudokuPuzzleCell.GENERATED);
-		puzzle.puzzle[3][8].setValue(8, SudokuPuzzleCell.GENERATED);
-		
-		puzzle.puzzle[4][0].setValue(9, SudokuPuzzleCell.GENERATED);
-		puzzle.puzzle[4][1].setValue(3, SudokuPuzzleCell.GENERATED);
-		puzzle.puzzle[4][2].setValue(4, SudokuPuzzleCell.GENERATED);
-		puzzle.puzzle[4][3].setValue(8, SudokuPuzzleCell.GENERATED);
-		puzzle.puzzle[4][4].setValue(1, SudokuPuzzleCell.GENERATED);
-		puzzle.puzzle[4][5].setValue(5, SudokuPuzzleCell.GENERATED);
-		puzzle.puzzle[4][6].setValue(2, SudokuPuzzleCell.GENERATED);
-		puzzle.puzzle[4][7].setValue(6, SudokuPuzzleCell.GENERATED);
-		puzzle.puzzle[4][8].setValue(7, SudokuPuzzleCell.GENERATED);
-		
-		puzzle.puzzle[5][0].setValue(8, SudokuPuzzleCell.GENERATED);
-		puzzle.puzzle[5][1].setValue(6, SudokuPuzzleCell.GENERATED);
-		puzzle.puzzle[5][2].setValue(7, SudokuPuzzleCell.GENERATED);
-		puzzle.puzzle[5][3].setValue(9, SudokuPuzzleCell.GENERATED);
-		puzzle.puzzle[5][4].setValue(3, SudokuPuzzleCell.GENERATED);
-		puzzle.puzzle[5][5].setValue(2, SudokuPuzzleCell.GENERATED);
-		puzzle.puzzle[5][6].setValue(5, SudokuPuzzleCell.GENERATED);
-		puzzle.puzzle[5][7].setValue(1, SudokuPuzzleCell.GENERATED);
-		puzzle.puzzle[5][8].setValue(4, SudokuPuzzleCell.GENERATED);
-		
-		puzzle.puzzle[6][0].setValue(2, SudokuPuzzleCell.GENERATED);
-		puzzle.puzzle[6][1].setValue(4, SudokuPuzzleCell.GENERATED);
-		puzzle.puzzle[6][2].setValue(8, SudokuPuzzleCell.GENERATED);
-		puzzle.puzzle[6][3].setValue(3, SudokuPuzzleCell.GENERATED);
-		puzzle.puzzle[6][4].setValue(7, SudokuPuzzleCell.GENERATED);
-		puzzle.puzzle[6][5].setValue(1, SudokuPuzzleCell.GENERATED);
+//		puzzle.puzzle[0][0].setValue(1, SudokuPuzzleCell.GENERATED);
+//		puzzle.puzzle[0][1].setValue(2, SudokuPuzzleCell.GENERATED);
+//		puzzle.puzzle[0][2].setValue(3, SudokuPuzzleCell.GENERATED);
+//		puzzle.puzzle[0][3].setValue(4, SudokuPuzzleCell.GENERATED);
+//		puzzle.puzzle[0][4].setValue(5, SudokuPuzzleCell.GENERATED);
+//		puzzle.puzzle[0][5].setValue(6, SudokuPuzzleCell.GENERATED);
+//		puzzle.puzzle[0][6].setValue(7, SudokuPuzzleCell.GENERATED);
+//		puzzle.puzzle[0][7].setValue(8, SudokuPuzzleCell.GENERATED);
+//		puzzle.puzzle[0][8].setValue(9, SudokuPuzzleCell.GENERATED);
+//		
+//		puzzle.puzzle[1][0].setValue(7, SudokuPuzzleCell.GENERATED);
+//		puzzle.puzzle[1][1].setValue(8, SudokuPuzzleCell.GENERATED);
+//		puzzle.puzzle[1][2].setValue(9, SudokuPuzzleCell.GENERATED);
+//		puzzle.puzzle[1][3].setValue(1, SudokuPuzzleCell.GENERATED);
+//		puzzle.puzzle[1][4].setValue(2, SudokuPuzzleCell.GENERATED);
+//		puzzle.puzzle[1][5].setValue(3, SudokuPuzzleCell.GENERATED);
+//		puzzle.puzzle[1][6].setValue(4, SudokuPuzzleCell.GENERATED);
+//		puzzle.puzzle[1][7].setValue(5, SudokuPuzzleCell.GENERATED);
+//		puzzle.puzzle[1][8].setValue(6, SudokuPuzzleCell.GENERATED);
+//		
+//		puzzle.puzzle[2][0].setValue(4, SudokuPuzzleCell.GENERATED);
+//		puzzle.puzzle[2][1].setValue(5, SudokuPuzzleCell.GENERATED);
+//		puzzle.puzzle[2][2].setValue(6, SudokuPuzzleCell.GENERATED);
+//		puzzle.puzzle[2][3].setValue(7, SudokuPuzzleCell.GENERATED);
+//		puzzle.puzzle[2][4].setValue(8, SudokuPuzzleCell.GENERATED);
+//		puzzle.puzzle[2][5].setValue(9, SudokuPuzzleCell.GENERATED);
+//		puzzle.puzzle[2][6].setValue(1, SudokuPuzzleCell.GENERATED);
+//		puzzle.puzzle[2][7].setValue(2, SudokuPuzzleCell.GENERATED);
+//		puzzle.puzzle[2][8].setValue(3, SudokuPuzzleCell.GENERATED);
+//		
+//		puzzle.puzzle[3][0].setValue(5, SudokuPuzzleCell.GENERATED);
+//		puzzle.puzzle[3][1].setValue(1, SudokuPuzzleCell.GENERATED);
+//		puzzle.puzzle[3][2].setValue(2, SudokuPuzzleCell.GENERATED);
+//		puzzle.puzzle[3][3].setValue(6, SudokuPuzzleCell.GENERATED);
+//		puzzle.puzzle[3][4].setValue(4, SudokuPuzzleCell.GENERATED);
+//		puzzle.puzzle[3][5].setValue(7, SudokuPuzzleCell.GENERATED);
+//		puzzle.puzzle[3][6].setValue(9, SudokuPuzzleCell.GENERATED);
+//		puzzle.puzzle[3][7].setValue(3, SudokuPuzzleCell.GENERATED);
+//		puzzle.puzzle[3][8].setValue(8, SudokuPuzzleCell.GENERATED);
+//		
+//		puzzle.puzzle[4][0].setValue(9, SudokuPuzzleCell.GENERATED);
+//		puzzle.puzzle[4][1].setValue(3, SudokuPuzzleCell.GENERATED);
+//		puzzle.puzzle[4][2].setValue(4, SudokuPuzzleCell.GENERATED);
+//		puzzle.puzzle[4][3].setValue(8, SudokuPuzzleCell.GENERATED);
+//		puzzle.puzzle[4][4].setValue(1, SudokuPuzzleCell.GENERATED);
+//		puzzle.puzzle[4][5].setValue(5, SudokuPuzzleCell.GENERATED);
+//		puzzle.puzzle[4][6].setValue(2, SudokuPuzzleCell.GENERATED);
+//		puzzle.puzzle[4][7].setValue(6, SudokuPuzzleCell.GENERATED);
+//		puzzle.puzzle[4][8].setValue(7, SudokuPuzzleCell.GENERATED);
+//		
+//		puzzle.puzzle[5][0].setValue(8, SudokuPuzzleCell.GENERATED);
+//		puzzle.puzzle[5][1].setValue(6, SudokuPuzzleCell.GENERATED);
+//		puzzle.puzzle[5][2].setValue(7, SudokuPuzzleCell.GENERATED);
+//		puzzle.puzzle[5][3].setValue(9, SudokuPuzzleCell.GENERATED);
+//		puzzle.puzzle[5][4].setValue(3, SudokuPuzzleCell.GENERATED);
+//		puzzle.puzzle[5][5].setValue(2, SudokuPuzzleCell.GENERATED);
+//		puzzle.puzzle[5][6].setValue(5, SudokuPuzzleCell.GENERATED);
+//		puzzle.puzzle[5][7].setValue(1, SudokuPuzzleCell.GENERATED);
+//		puzzle.puzzle[5][8].setValue(4, SudokuPuzzleCell.GENERATED);
+//		
+//		puzzle.puzzle[6][0].setValue(2, SudokuPuzzleCell.GENERATED);
+//		puzzle.puzzle[6][1].setValue(4, SudokuPuzzleCell.GENERATED);
+//		puzzle.puzzle[6][2].setValue(8, SudokuPuzzleCell.GENERATED);
+//		puzzle.puzzle[6][3].setValue(3, SudokuPuzzleCell.GENERATED);
+//		puzzle.puzzle[6][4].setValue(7, SudokuPuzzleCell.GENERATED);
+//		puzzle.puzzle[6][5].setValue(1, SudokuPuzzleCell.GENERATED);
 //		puzzle.puzzle[6][6].setValue(6, SudokuPuzzleCell.GENERATED);
 //		puzzle.puzzle[6][7].setValue(9, SudokuPuzzleCell.GENERATED);
 //		puzzle.puzzle[6][8].setValue(5, SudokuPuzzleCell.GENERATED);
@@ -118,9 +118,6 @@ public class PuzzleTester1 {
 			System.out.println("puzzle2 is null");
 		else
 		puzzle2.printPuzzle();
-		
-
-		
 
 	}
 	
@@ -166,11 +163,47 @@ public class PuzzleTester1 {
 			System.out.println("puzzle2 is null");
 		else
 		puzzle2.printPuzzle();
+	}
+	
+	void runPuzzle4() {
+		SudokuPuzzle puzzle = new SudokuPuzzle();
+		puzzle.puzzle[0][0].setValue(9, SudokuPuzzleCell.GENERATED);
+		puzzle.puzzle[0][2].setValue(3, SudokuPuzzleCell.GENERATED);
+		puzzle.puzzle[0][3].setValue(5, SudokuPuzzleCell.GENERATED);
+		puzzle.puzzle[0][4].setValue(4, SudokuPuzzleCell.GENERATED);
+		puzzle.puzzle[2][1].setValue(1, SudokuPuzzleCell.GENERATED);
+		puzzle.puzzle[2][4].setValue(8, SudokuPuzzleCell.GENERATED);
+		puzzle.puzzle[2][6].setValue(4, SudokuPuzzleCell.GENERATED);
+		puzzle.puzzle[2][7].setValue(3, SudokuPuzzleCell.GENERATED);
+		puzzle.puzzle[3][0].setValue(5, SudokuPuzzleCell.GENERATED);
+		puzzle.puzzle[3][2].setValue(9, SudokuPuzzleCell.GENERATED);
+		puzzle.puzzle[3][8].setValue(1, SudokuPuzzleCell.GENERATED);
+		puzzle.puzzle[4][1].setValue(3, SudokuPuzzleCell.GENERATED);
+		puzzle.puzzle[4][4].setValue(1, SudokuPuzzleCell.GENERATED);
+		puzzle.puzzle[4][7].setValue(8, SudokuPuzzleCell.GENERATED);
+		puzzle.puzzle[5][0].setValue(8, SudokuPuzzleCell.GENERATED);
+		puzzle.puzzle[5][6].setValue(2, SudokuPuzzleCell.GENERATED);
+		puzzle.puzzle[5][8].setValue(4, SudokuPuzzleCell.GENERATED);
+		puzzle.puzzle[6][1].setValue(8, SudokuPuzzleCell.GENERATED);
+		puzzle.puzzle[6][2].setValue(7, SudokuPuzzleCell.GENERATED);
+		puzzle.puzzle[6][4].setValue(5, SudokuPuzzleCell.GENERATED);
+		puzzle.puzzle[6][7].setValue(4, SudokuPuzzleCell.GENERATED);
+		puzzle.puzzle[8][4].setValue(2, SudokuPuzzleCell.GENERATED);
+		puzzle.puzzle[8][5].setValue(8, SudokuPuzzleCell.GENERATED);
+		puzzle.puzzle[8][6].setValue(6, SudokuPuzzleCell.GENERATED);
+		puzzle.puzzle[8][8].setValue(9, SudokuPuzzleCell.GENERATED);
 		
-		int[] errors = {0};
-		for (int i : errors) {
-			SudokuSolver.Constraint constraint2 = SudokuSolver.Constraint.IntToConstraint(i);
-			constraint2.printConstraint();
+		puzzle.printPuzzle();
+		SudokuSolver solver1 = new SudokuSolver();
+		System.out.println("Solving....");
+		Quant sols = solver1.solutionsM(puzzle);
+		switch(sols) {
+			case NONE: System.out.println("No Solutions"); break;
+			case ONE: System.out.println("One Solution"); break;
+			case MULTIPLE: System.out.println("Multiple Solutions"); break;
 		}
+		SudokuPuzzle puzzle2 = solver1.solvePuzzle(puzzle);
+		puzzle2.printPuzzle();
+
 	}
 }
