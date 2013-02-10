2fc0090a57e6ac0d93c5b8d45181727309354e24
riklas <rikins21@gmail.com>
2013-02-10 23:19:22 +0000

    Added generated puzzles class

---
+++ RHRSudoku/src/com/example/rhrsudoku/GeneratedPuzzles.java
@@ -0,0 +1,22 @@
+package com.example.rhrsudoku;
+
+public class GeneratedPuzzles implements SudokuGenerator {
+
+	public GeneratedPuzzles() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public SudokuPuzzle getPuzzle(int difficulty) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public SudokuPuzzleCell[][] setValues(SudokuPuzzleCell[][] targetPuzzle,
+			int[][] hardcodePuzzle) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
