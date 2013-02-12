48f310895f41000d2a182da6ea7a63fe89d89c6e
Hassan Ali <hassan.ali.3821@gmail.com>
2013-02-12 01:44:26 +0000

    updated digitbutton

--- RHRSudoku/src/com/example/rhrsudoku/DigitButton.java
+++ RHRSudoku/src/com/example/rhrsudoku/DigitButton.java
@@ -2,6 +2,7 @@ package com.example.rhrsudoku;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
@@ -34,6 +35,18 @@ public class DigitButton extends Button {
 	
 	final private void createPaints() {
 		paint1 = new Paint();
+		paint2 = new Paint();
+		paint3 = new Paint();
+		paint4 = new Paint();
+		paint5 = new Paint();
+		paint6 = new Paint();
+		
+		paint1.setColor(Color.LTGRAY);
+		paint2.setColor(0x0033B5E5);
+		paint3.setColor(0x000099CC);
+		paint4.setColor(0x00FFBB33);
+		paint5.setColor(0x00FF8800);
+		paint6.setColor(Color.BLACK);
 	}
 	
 }
