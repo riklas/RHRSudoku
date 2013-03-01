963667aeb6ea8af2375f3b01e013b677d15d8c83
Hassan Ali <hassan.ali.3821@gmail.com>
2013-03-01 13:17:52 +0000

    added custom layout for drawing gridlines

---
+++ RHRSudoku/src/com/example/rhrsudoku/SudokuPuzzleLayout.java
@@ -0,0 +1,14 @@
+package com.example.rhrsudoku;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.GridLayout;
+
+public class SudokuPuzzleLayout extends GridLayout {
+
+	public SudokuPuzzleLayout(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		// TODO Auto-generated constructor stub
+	}
+
+}
