32dc0c56ac93637d6ccb87c8b2a991529bdd0e8b
Hassan Ali <hassan.ali.3821@gmail.com>
2013-03-08 15:08:16 +0000

    updated

--- RHRSudoku/src/com/example/rhrsudoku/DifficultyChooser.java
+++ RHRSudoku/src/com/example/rhrsudoku/DifficultyChooser.java
@@ -13,7 +13,7 @@ public class DifficultyChooser extends Activity {
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
+		super.onCreate(savedInstanceState); 
 		setContentView(R.layout.activity_dfchooser);
 	}
 
